c426005233666983579da14d70888a4624c57858
MuratEsendemir <esendemirm86@gmail.com>
2025-09-30 16:55:57 +0300

    Add files via upload

---
+++ Cards.java
@@ -0,0 +1,87 @@
+package cardgame;
+import java.util.Random;
+//Murat Esendemir
+
+// Cards - Maintains a collection of zero or more playing cards.
+//         Provides facilities to create a full pack of 52 cards
+//         and to shuffle the cards.
+// author:
+// date:
+public class Cards
+{
+    final int NOOFCARDSINFULLPACK = 52;
+    
+    // properties
+    Card[] cards;
+    int    valid;  // number of cards currently in collection
+    
+    // constructors
+    public Cards( boolean fullPack)
+    {
+        cards = new Card[ NOOFCARDSINFULLPACK ];
+        valid = 0;
+        
+        if ( fullPack)
+            createFullPackOfCards();
+    }
+    
+    // methods
+    public Card getTopCard()
+    {
+        Card tmp;
+
+        if ( valid <= 0)
+            return null;
+        else
+        {
+            valid--;
+            tmp = cards[valid];
+            cards[valid] = null;
+            return tmp;
+        }
+    }
+    
+    public boolean addTopCard( Card c)
+    {
+        if ( valid < cards.length)
+        {
+            cards[valid] = c;   // should this be cloned?
+            valid++;
+            return true;
+        }
+        return false;
+    }
+    
+    private void createFullPackOfCards()
+    {
+        for (int suit = 0; suit < 4; suit++) {
+            for (int faceValue = 0; faceValue < 13; faceValue++) {
+            addTopCard(new Card(faceValue, suit));
+            }
+        }
+    }
+    
+    public void shuffle()
+    {
+        Random rand = new Random();
+
+        for (int i = valid - 1; i > 0; i--) {
+            int j = rand.nextInt(i + 1);
+            
+            Card temp = cards[i];
+            cards[i] = cards[j];
+            cards[j] = temp;
+        }
+    }
+    
+    
+    // For testOnly... remove from production version!
+    public void testOnlyPrint()
+    {
+        for ( int i =0; i < valid; i++)
+        {
+            System.out.println( cards[i] );
+        }
+    }
+    
+} // end class Cards
